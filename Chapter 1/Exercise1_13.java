6de15bc58199561570cfd33e3c595d9e873ca37a
EliseSarles <60886214+EliseSarles@users.noreply.github.com>
2020-03-05 07:20:31 -0700

    Added Correct 1_13

---
+++ Chapter 1/Exercise1_13.java
@@ -0,0 +1,7 @@
+//Elise Sarles March 4 2020
+class Exercise1_13 {
+	public static void main(String[] args) {
+		System.out.println("X =" + (((44.5 * 0.55) - (50.2 * 5.9)) / ((3.4 * 0.55) - (50.2 * 2.1))));
+		System.out.println("Y =" + (((3.4 * 5.9) - (44.5 * 2.1)) / ((3.4 * 0.55)-(50.2 * 2.1))));
+	}
+}
