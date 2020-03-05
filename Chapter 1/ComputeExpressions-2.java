b0db1ad594d87211a2b8dd969c393ca2b7f61c10
EliseSarles <60886214+EliseSarles@users.noreply.github.com>
2020-03-05 07:15:22 -0700

    Added Chapter 1 & work from it

---
+++ Chapter 1/ComputeExpressions-2.java
@@ -0,0 +1,6 @@
+//Elise Sarles- Feb. 26, 2020. First ever real code in the class I wrote.
+class ComputeExpression {
+	public static void main(String[] args) { 
+		System.out.println ((9.5*4.5-2.5*3) / (45.5-3.5));
+	}
+}
---
+++ Chapter 1/Exercise 1-9.java
@@ -0,0 +1,8 @@
+//Elise Sarles Feb. 27 2020
+class Ex_1_9 {
+	public static void main(String[] args) {
+		System.out.println("Area: " + (4.5 * 7.9));
+		System.out.println("Perimeter: " + ( 4.5 * 2 + 7.9 * 2 ));
+		
+	}
+}
